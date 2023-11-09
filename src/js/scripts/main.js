db8ef3a649206feb33ca4daf851b48d2e5504e53
Louis Pallett <louis.pallett@outlook.com>
2023-11-09 17:42:38 +0000

    Initial file setup

---
+++ src/js/scripts/main.js
@@ -0,0 +1,9 @@
+import '../../scss/styles.scss';
+
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+
+console.log("The answer is: " + add(5, 8));
+
+export { add };
---
+++ src/js/scripts/main.test.js
@@ -0,0 +1,5 @@
+import { add } from "./main";
+
+test("Adds", () => {
+    expect(add(5, 3)).toBe(8);
+})

2ee9a9d3efa4868436d43cf780537c304e1a1956
Louis Pallett <louis.pallett@outlook.com>
2023-11-11 12:09:16 +0000

    Added direction property to Ship class

--- src/js/scripts/classes.js
+++ src/js/scripts/classes.js
@@ -1,42 +1,6 @@
-import { checkBoard } from "./helperfunctions";
+import { checkBoard, checkPosition } from "./helperfunctions";
 
 class Gameboard {
-    /*
-    ================================================
-    Pseudocode
-    ================================================
-    >>>>Gameboards should be able to place ships at specific coordinates by calling the ship class<<<<
-
-        >>>Logic
-        >Ingredients:
-        A ship (which includes it's size)
-        The coordinates (starting coordinate + size to determine final)
-
-        >Checks:
-        Is it on the board? Hark back to knight travails
-        
-        >Testing
-            >Place a ship at legal coordinates - does it return coordinates correctly?
-            >Place a ship at illegal coordinates - does it return an error?
-
-        >>Code<<
-        Find the index of the board.
-            Below helper function? If(checkBoard)
-                >>>Loop through size number of times
-                >>>If "horizontal"
-                >>>    return check board[index + (i * 10)]; (return true if space is free);
-                >>>Else if "vertical"
-                >>>    return check board[index + (i)]; (return true if space is free);
-                >>>return false;
-            
-
-
-
-    Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, 
-    or records the coordinates of the missed shot.
-    Gameboards should keep track of missed attacks so they can display them properly.
-    Gameboards should be able to report whether or not all of their ships have been sunk.
-    */
     constructor(size) {
         this.size = size;
         this.board = this.buildBoard(size);
@@ -53,7 +17,9 @@ class Gameboard {
     }
 
     placeShip = (board, index, size, direction) => {
-        if(checkBoard(board, index, size, direction)) {
+        const boardIsFree = checkBoard(board, index, size, direction);
+        const positionIsLegal = checkPosition();
+        if(boardIsFree && positionIsLegal) {
             return true;
         }
         return false;
@@ -61,27 +27,9 @@ class Gameboard {
 }
 
 class Ship {
-    /*
-    ================================================
-    Pseudocode
-    ================================================
-    Begin your app by creating the Ship class/factory (your choice).
-
-        Your ‘ships’ will be objects that include their length, the number of times they’ve been hit and whether or not they’ve been sunk.
-
-            >>> Represented in a class, constructor with length (parameter), hitNum = 0, and sunk = false.
-
-        REMEMBER you only have to test your object’s public interface. Only methods or properties that are used outside of your ‘ship’ object need unit tests.
-        Ships should have a hit() function that increases the number of ‘hits’ in your ship.
-
-            >> A simple increment
-
-        isSunk() should be a function that calculates whether a ship is considered sunk based on its length and the number of hits it has received.
-
-            >> A simple conditional
-    */
-    constructor(size) {
+    constructor(size, direction = "horizontal") {
         this.size = size;
+        this.direction = direction;
         this.sunk = false;
         this.hitNum = 0;
     }

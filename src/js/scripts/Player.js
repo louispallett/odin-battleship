5c7449eadf353b45f46a94d45b8708421b66f6b2
Louis Pallett <louis.pallett@outlook.com>
2023-11-12 23:25:23 +0000

    Human and Computer classes

--- src/js/scripts/Player.js
+++ src/js/scripts/Player.js
@@ -1,9 +1,30 @@
-export { Player };
+import { checkSquare } from "./helperfunctions";
+export { Player, Computer, Human };
 
 class Player {
     constructor() {}
+};
 
+class Human extends Player {
+    constructor() {
+        super();
+    };
+    
     play = (gameboard, index) => {
         return gameboard.receiveAttack(gameboard, index);
     }
-};
+}
+
+class Computer extends Player {
+    constructor() {
+        super();
+    };
+
+    play = (gameboard) => {
+        let index = 0;
+        while(!checkSquare(gameboard, index)) {
+            index = Math.floor(Math.random() * 100);
+        }
+        return gameboard.receiveAttack(gameboard, index);
+    }
+}

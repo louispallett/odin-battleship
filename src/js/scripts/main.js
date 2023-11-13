f51fe7dab233947ea25c453bbee693f6d235f21d
Louis Pallett <louis.pallett@outlook.com>
2023-11-13 22:26:18 +0000

    createGrid() and handleGridClick() form grid on DOM linked to Gameboard class

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -1,1 +1,39 @@
 import '../../scss/styles.scss';
+import { Gameboard } from "./Gameboard";
+import { Ship } from "./Ship";
+import { Computer, Human } from "./Player";
+
+const createGrid = (gameboard, gridElement) => {
+    gridElement.innerHTLM = "";
+    gridElement.setAttribute("id", "gameboard-grid");
+
+    for(let i = 0; i < gameboard.size * gameboard.size; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.dataset.index = i;
+        gridItem.addEventListener("click", () => handleGridClick(gameboard, i, gridItem));
+        gridElement.appendChild(gridItem);
+        
+        if(gameboard.board[i] === 1) {
+            // if grid has been hit (just an example here)
+            gridItem.style.backgroundColor = "pink";
+        } else if (gameboard.board[i] instanceof Ship) {
+            gridItem.style.backgroundColor = "green";
+        }
+    }
+}
+
+const handleGridClick = (gameboard, index, gridItem) => {
+    const result = gameboard.receiveAttack(gameboard, index);
+    if(result) {
+        gridItem.style.backgroundColor = "red";
+    }
+}
+
+const playerGameboard = new Gameboard(10);
+const computerGameboard = new Gameboard(10);
+
+const playerGridElement = document.getElementById('player-grid');
+const computerGridElement = document.getElementById('computer-grid');
+
+createGrid(playerGameboard, playerGridElement);
+createGrid(computerGameboard, computerGridElement); 

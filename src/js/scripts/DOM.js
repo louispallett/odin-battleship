2ae4c8eec6213c04cc28f356ebbea301d941c477
Louis Pallett <louis.pallett@outlook.com>
2023-12-06 19:23:18 +0000

    Code restructure - functions moved to DOM.js

---
+++ src/js/scripts/DOM.js
@@ -0,0 +1,90 @@
+import { Ship } from "./Ship";
+export { addAttackFunctionality, attack, createGrid };
+
+let turnCounter = 1;
+
+const createGrid = (gameboard, gridElement, playing = "computer") => {
+    gridElement.innerHTLM = "";
+
+    for(let i = 0; i < 100; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.dataset.index = i;
+        gridItem.dataset.class = gameboard.board[i];
+        
+        if(playing == "human") {
+            gridItem.addEventListener("click", () => {
+                if(attack.alreadyClicked(gridItem)) return;
+                attack.attackResult(gameboard, i, gridItem)
+                turnCounter++;
+            });
+        } else {
+            if (gameboard.board[i] instanceof Ship) {
+                gridItem.style.backgroundColor = "green";
+            }
+        }
+        gridElement.appendChild(gridItem);
+    }
+}
+
+const attack = (() => {
+    const attackResult = (gameboard, index, gridItem) => {
+        const hitResult = gameboard.receiveAttack(index);
+
+        if(alreadyClicked(gridItem)) return;
+
+        if(hitResult) {
+            success(gameboard, index, gridItem);
+        } else {
+            miss(gridItem);
+        }
+    };
+    
+    const success = (gameboard, index, gridItem) => {
+        gridItem.dataset.class = 0;
+        gridItem.style.backgroundColor = "red";
+        gameboard.board[index] = 1;
+        if(gameboard.haveLost()) {
+            gameOver(gameboard);
+        }
+    };
+
+    const miss = (gridItem) => {
+        gridItem.dataset.class = 1;
+        gridItem.style.backgroundColor = "pink";
+    };
+
+    const alreadyClicked = (gridItem) => {
+        return(gridItem.dataset.class == 1 ||
+            gridItem.dataset.class == 0)? true : false;
+    };
+
+    return { attackResult, alreadyClicked };
+})();
+
+const gameOver = (losersGameboard) => {
+    console.log("STOP. Last index hit was: " + losersGameboard.hits[losersGameboard.hits.length - 1]);
+};
+
+const computerTurn = (playerGameboard, computer, playGrid) => {
+    if (turnCounter % 2 != 0) {
+        setTimeout(() => {
+            computerAttack(playerGameboard, computer, playGrid);
+        }, 0);
+    }
+};
+
+const computerAttack = (playerGameboard, computer, playGrid) => {
+    let index = computer.play(playerGameboard);
+    attack.attackResult(playerGameboard, index, playGrid.children[index]);
+};
+
+const addAttackFunctionality = (compGrid, playerGameboard, computerGameboard, computer, playGrid) => {
+    compGrid.addEventListener("click", (event) => {
+        if (turnCounter % 2 == 0) {
+            const index = event.target.dataset.index;
+            attack.attackResult(computerGameboard, index, event.target);
+            turnCounter++;
+            computerTurn(playerGameboard, computer, playGrid);
+        }
+    });
+}

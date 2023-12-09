08c24380256b4d68b0be6e6c633313ac14db77d7
Louis Pallett <louis.pallett@outlook.com>
2023-12-09 17:50:04 +0000

    Created DOM pre_game() and createGrid()

--- src/js/scripts/main.js
+++ src/js/scripts/main.js
@@ -2,4 +2,8 @@ import '../../scss/styles.scss';
 import { Gameboard } from "./Gameboard";
 import { Computer, Human } from './Player';
 import { Ship } from "./Ship";
+import { pre_game } from './DOM';
 // import { addAttackFunctionality, createGrid } from './DOM';
+
+const playBtn = document.getElementById("playbtn");
+playBtn.addEventListener("click", () => {pre_game()})
